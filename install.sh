28b95d3e17850ab7b15c45ff7f68f184af413842
Jon Chesterfield <jonathanchesterfield@gmail.com>
2022-08-09 14:43:11 +0100

    update build scripts, going to try following amd-stg-open device libs instead of a rocm release

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -x
 set -e
 set -o pipefail
 
+cd $HOME/.emacs.d/build_scripts # pain to find the actual dir
 for f in amdbuild aomp_tests common_build debbuild relbuild; do
     rm -f ../bin/$f
     ln $f ../bin/$f
